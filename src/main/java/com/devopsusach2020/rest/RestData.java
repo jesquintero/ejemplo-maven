193df29e8fdf3af6a8def911818a69d9ef9b98a0
Jesus Quintero <jquintero@kuantum.cl>
2022-01-11 21:18:32 -0300

    reparando comentarios

--- src/main/java/com/devopsusach2020/rest/RestData.java
+++ src/main/java/com/devopsusach2020/rest/RestData.java
@@ -17,7 +17,7 @@ import com.devopsusach2020.model.Pais;
 import com.devopsusach2020.model.Mundial;
 import com.google.gson.Gson;
 
-/* Definición del controlador */
+
 @RestController
 @RequestMapping(path = "/rest/mscovid")
 public class RestData {
@@ -35,7 +35,7 @@ public class RestData {
 		return response;
 	}
 	
-	/* Estados del pais */
+	
 	@GetMapping(path = "/estadoPais", produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody Pais getTotalPais(@RequestParam(name = "pais") String message){
 		RestTemplate restTemplate = new RestTemplate();
@@ -67,7 +67,7 @@ public class RestData {
 		return response;		
 	}
 	
-	/* Estados del mundo */
+	
 	@GetMapping(path = "/estadoMundial", produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody Mundial getTotalMundial(){
 		
